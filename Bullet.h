a00c0f187716bb29a1bfe7a01d52e70b58d37eae
NEKRASOV <146654192+LOOK-MOM-I-CAN-FLY@users.noreply.github.com>
2024-09-30 22:09:00 +0300

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,22 +1,28 @@
 #ifndef BULLET_H
 #define BULLET_H
 
-#include "/Users/sindi_hall/Downloads/SFML_ROOT/include/SFML/Graphics.hpp"
-using namespace sf;
+#include "SFML/Graphics.hpp"
+#include <vector>
+#include <iostream>
 
 class Bullet {
 public:
-    float x, y;         // Координаты пули
-    float dx, dy;       // Направление пули
-    float speed;        // Скорость пули
-    float lifetime;     // Время жизни пули
-    bool isAlive;       // Статус пули (активна или уничтожена)
-    Sprite sprite;      // Спрайт пули
-    Texture texture;    // Текстура пули (добавлено)
-
     Bullet(float X, float Y, float Dx, float Dy);  // Конструктор
 
-    void update(float time);  // Обновление состояния пули
+    void update(float time, const std::vector<sf::FloatRect>& solidObjects);  // Обновление состояния пули
+
+    bool isAlive() const { return alive; }  // Статус пули (жива или уничтожена)
+    const sf::Sprite& getSprite() const { return sprite; }  // Возвращает спрайт пули
+
+private:
+    float x, y;         // Координаты пули
+    float dx, dy;       // Направление пули
+    float lifetime;     // Время жизни пули
+    bool alive;         // Флаг активности пули
+    sf::Sprite sprite;  // Спрайт пули
+    sf::Texture texture; // Текстура пули
+
+    void checkCollisionWithMap(const std::vector<sf::FloatRect>& solidObjects);  // Проверка столкновений с объектами карты
 };
 
 #endif
